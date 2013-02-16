69e5edd0bba78821e0f8fae5b4c3631dcea4f0db
Yecheng Fu <cofyc.jackson@gmail.com>
2013-02-16 17:49:09 +0800

    fix test

--- test.sh
+++ test.sh
@@ -8,7 +8,7 @@ path: /path/to/file
 argc: 1
 argv[0]: a'
 
-is "$(./test_argparse -f -f --force 2>&1)" 'force: 3'
+is "$(./test_argparse -f -f --force --no-force 2>&1)" 'force: 2'
 
 is "$(./test_argparse -n 2>&1)" 'error: option `n` requires a value'
 
@@ -17,17 +17,6 @@ is "$(./test_argparse -n 2 2>&1)" 'num: 2'
 is "$(./test_argparse -n2 2>&1)" 'num: 2'
 
 is "$(./test_argparse -na 2>&1)" 'error: option `n` expects a numerical value'
-
-is "$(./test_argparse --help 2>&1)" 'Usage: test_argparse [options] [[--] args]
-
-    -h, --help    show this help message and exit
-    -f, --force   force to do
-    -t, --test    test only
-    -p, --path    path to read
-    -n, --num     selected num
-    --read        read perm
-    --write       write perm
-    --exec        exec perm'
 
 is "$(./test_argparse -f -- do -f -h 2>&1)" 'force: 1
 argc: 3

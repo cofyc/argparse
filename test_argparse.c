35864263f59a65f56149aabbbdb096cff9ab373a
Christoffer Gustafsson <christoffer.golf@gmail.com>
2018-05-25 13:12:11 +0200

    Fix warnings in test
    As described in issue #18, running make test on gcc produces many
    warnings. This patch removes the warnings.

--- test_argparse.c
+++ test_argparse.c
@@ -25,15 +25,15 @@ main(int argc, const char **argv)
     struct argparse_option options[] = {
         OPT_HELP(),
         OPT_GROUP("Basic options"),
-        OPT_BOOLEAN('f', "force", &force, "force to do"),
-        OPT_BOOLEAN('t', "test", &test, "test only"),
-        OPT_STRING('p', "path", &path, "path to read"),
-        OPT_INTEGER('i', "int", &int_num, "selected integer"),
-        OPT_FLOAT('s', "float", &flt_num, "selected float"),
+        OPT_BOOLEAN('f', "force", &force, "force to do", NULL, 0, 0),
+        OPT_BOOLEAN('t', "test", &test, "test only", NULL, 0, 0),
+        OPT_STRING('p', "path", &path, "path to read", NULL, 0, 0),
+        OPT_INTEGER('i', "int", &int_num, "selected integer", NULL, 0, 0),
+        OPT_FLOAT('s', "float", &flt_num, "selected float", NULL, 0, 0),
         OPT_GROUP("Bits options"),
         OPT_BIT(0, "read", &perms, "read perm", NULL, PERM_READ, OPT_NONEG),
-        OPT_BIT(0, "write", &perms, "write perm", NULL, PERM_WRITE),
-        OPT_BIT(0, "exec", &perms, "exec perm", NULL, PERM_EXEC),
+        OPT_BIT(0, "write", &perms, "write perm", NULL, PERM_WRITE, 0),
+        OPT_BIT(0, "exec", &perms, "exec perm", NULL, PERM_EXEC, 0),
         OPT_END(),
     };
 

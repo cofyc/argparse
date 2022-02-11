4c253942b136608a23bba5b86677e1a778b34e03
Yecheng Fu <cofyc.jackson@gmail.com>
2022-02-11 10:49:45 +0800

    subcommand: must pass ARGPARSE_STOP_AT_NON_OPTION to argparse_init in main program

--- tests/subcommands.sh
+++ tests/subcommands.sh
@@ -4,4 +4,18 @@
 plan_no_plan
 
 is "$(./subcommands foo)" 'executing subcommand foo
-argv[0]: foo'
+argc: 1
+argv[0]: foo
+after argparse_parse:
+argc: 0'
+
+is "$(./subcommands foo -t -p /path/to/file arg1)" 'executing subcommand foo
+argc: 5
+argv[0]: foo
+argv[1]: -t
+argv[2]: -p
+argv[3]: /path/to/file
+argv[4]: arg1
+after argparse_parse:
+argc: 1
+argv[0]: arg1'
